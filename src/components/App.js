63f342d22974b34e5dd76cad5e88d91353ba4345
Dylan Baker <dylanmbaker@yahoo.com>
2022-01-17 20:13:29 -0600

    added applyNumber eventhandlers to all numbers

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React, { useReducer } from 'react';
 import reducer, { initialState } from '../reducers';
-import { addOne } from '../actions';
 import { applyNumber } from '../actions';
 
 import './App.css';
@@ -38,21 +37,21 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1} onClick={addNumberHandler(1)} />
-              <CalcButton value={2}/>
-              <CalcButton value={3}/>
+              <CalcButton value={1} onClick={() => addNumberHandler(1)} />
+              <CalcButton value={2} onClick={() => addNumberHandler(2)} />
+              <CalcButton value={3} onClick={() => addNumberHandler(3)} />
             </div>
 
             <div className="row">
-              <CalcButton value={4}/>
-              <CalcButton value={5}/>
-              <CalcButton value={6}/>
+              <CalcButton value={4} onClick={() => addNumberHandler(4)} />
+              <CalcButton value={5} onClick={() => addNumberHandler(5)} />
+              <CalcButton value={6} onClick={() => addNumberHandler(6)} />
             </div>
 
             <div className="row">
-              <CalcButton value={7}/>
-              <CalcButton value={8}/>
-              <CalcButton value={9}/>
+              <CalcButton value={7} onClick={() => addNumberHandler(7)} />
+              <CalcButton value={8} onClick={() => addNumberHandler(8)} />
+              <CalcButton value={9} onClick={() => addNumberHandler(9)} />
             </div>
 
             <div className="row">
